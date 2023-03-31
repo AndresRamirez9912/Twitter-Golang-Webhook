803fb5050515d29751e9013e537d6377a2444048
ByAlex#15 <83742525+Alex155920@users.noreply.github.com>
2023-03-31 13:31:10 -0500

    Create a cron to check new messages (#7)
    * Create a cron to check new messages
    
    * Delete print and get SenderId

---
+++ src/cron/time.go
@@ -0,0 +1,54 @@
+package cron
+
+import (
+	"log"
+	"time"
+	"twitter-webhook/src/constants"
+	"twitter-webhook/src/database"
+	"twitter-webhook/src/models"
+	"twitter-webhook/src/services"
+)
+
+var Cont = 0
+
+func WaitMessages() chan bool {
+	ticker := time.NewTicker(5 * time.Second)
+	quit := make(chan bool, 1)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				report()
+			case <-quit:
+				ticker.Stop()
+			}
+		}
+	}()
+	return quit
+}
+
+func report() {
+
+	messages, err := services.LookDirectMessages()
+	//Handle Error o manejador de errores
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Comparar si son diferentes (iterar la cantidad de mensajes nuevos)
+	if Cont != messages.Meta.Result_count && messages.Data[0].Sender_id != constants.ID_BOT {
+		entry := models.TwitterField{
+			Id:            messages.Data[0].Sender_id,
+			CreatedAt:     time.Now().String(),
+			Active:        true,
+			LastMessageId: messages.Data[0].Id,
+		}
+		err := database.CreateItem(entry)
+		if err != nil {
+			log.Fatal(err)
+		}
+		Cont = messages.Meta.Result_count
+		// Enviar Respuesta al cliente
+		services.SendDirectMesage("1638652102212743168", "El bot del banco Popular te da la Bienvenida")
+	}
+}
