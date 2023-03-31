8bc8d8e29df7d3427ed5ea84dca056e66c8ce314
Andres Ramirez <andres.ramirez9912@gmail.com>
2023-03-31 00:19:28 -0500

    Modify auth to use query parameters

--- src/constants/directMessage.constants.go
+++ src/constants/directMessage.constants.go
@@ -1,4 +1,7 @@
 package constants
 
 const SEND_DIRECT_MESSAGE_ENDPOINT = "/2/dm_conversations/with/%s/messages"
+
 const LOOKUP_DIRECT_MESSAGES = "/2/dm_events"
+const DM_EVENT_FIELDS_QUERY = "dm_event.fields"
+const DM_EVENT_FIELDS_VALUE = "id,text,event_type,sender_id"
